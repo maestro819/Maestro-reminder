a2b2087d21fd3c107d3b19e4ffbcfe532131715c
Maestro <cjm77v2b2v@privaterelay.appleid.com>
2026-08-19 15:22:50 +0700

    Create AlarmReceiver.java

---
+++ app/src/main/java/com/maestro/reminder/AlarmReceiver.java
@@ -0,0 +1,22 @@
+package com.maestro.reminder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent alarmIntent = new Intent(context, AlarmScreenActivity.class);
+
+        alarmIntent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP
+        );
+
+        context.startActivity(alarmIntent);
+    }
+}
